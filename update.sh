567b1ab9ba7fe8f9d3c955fe228f7485e9d6014e
Alex Hawking <alexhawking23@gmail.com>
2023-11-14 16:01:31 +0800

    add record stuff

--- update.sh
+++ update.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-DATE=`date +%Y-%m-%d`
-echo "Today's date is $DATE" > README.md
+
+# Replace with your Discogs username and token
+USERNAME="ah33808"
+TOKEN=$1
+
+# Fetch collection data
+COLLECTION_URL="https://api.discogs.com/users/$USERNAME/collection/folders/0/releases"
+RESPONSE=$(curl -s -H "Authorization: Discogs token=$TOKEN" "$COLLECTION_URL")
+
+# Parse and get a random record
+RANDOM_RECORD=$(echo $RESPONSE | jq -r '.releases[] | "\(.basic_information.title) - \(.basic_information.artists[].name)"' | shuf -n 1)
+day_as_word=$(date +%A)
+echo "Hello and welcome to $day_as_word! \n Todays Album is: $RANDOM_RECORD" > README.md
