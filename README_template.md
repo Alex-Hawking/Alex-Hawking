cc05e8501e6d03bd88908de49d90f48c50efb34e
Alex Hawking <alexhawking23@gmail.com>
2023-11-14 16:18:46 +0800

    pre formatted

---
+++ README_template.md
@@ -0,0 +1,3 @@
+## Hello and welcome to **${DAY}**!
+
+Todays Album is: *${RANDOM_RECORD}*
--- update.sh
+++ update.sh
@@ -4,12 +4,22 @@
 USERNAME="ah33808"
 TOKEN=$1
 
+cat README_template.md > README.md
+
 # Fetch collection data
 COLLECTION_URL="https://api.discogs.com/users/$USERNAME/collection/folders/0/releases"
 RESPONSE=$(curl -s -H "Authorization: Discogs token=$TOKEN" "$COLLECTION_URL")
 
 # Parse and get a random record
 RANDOM_RECORD=$(echo $RESPONSE | jq -r '.releases[] | "\(.basic_information.title) - \(.basic_information.artists[].name)"' | shuf -n 1)
+
+echo $RANDOM_RECORD
+
 DAY=$(date +%A)
 
-cat README.md | sed "s/\\${DAY}/$DAY/g; s/\\${RANDOM_RECORD}/$RANDOM_RECORD/g" > README.md
+# Replace placeholders in a temporary file
+sed "s/\${DAY}/$DAY/g" README.md > README_tmp.md
+sed "s/\${RANDOM_RECORD}/$RANDOM_RECORD/g" README_tmp.md > README.md
+
+# Clean up the temporary file
+rm README_tmp.md
